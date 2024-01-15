31327731885191815e3c31ec627bda8cf442564b
carl.hummerhielm <carl.hummerhielm@elev.ga.ntig.se>
2024-01-15 09:33:01 +0100

    ...

--- src/LogIn.java
+++ src/LogIn.java
@@ -1,13 +1,41 @@
 import java.util.ArrayList;
 
 public class LogIn {
+    //Dessa två Array listor lagrar all inloggnings information
     private ArrayList<Integer> employeeID = new ArrayList<>();
     private ArrayList<String> password = new ArrayList<>();
 
-    public int getEmployeeID(){
-
+    //Validerar så att EmployeeID och password tas in i metoden stämmer med det som är i array listorna
+    public boolean validate(int employeeID, String password) {
+        if (this.employeeID.contains(employeeID) && this.password.contains(password)) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    public int getPassword(){
+
+    public boolean logIn(int employeeID, String password) {
+
+        if (this.password.get(getEmployeeID(employeeID)) == password) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //hämtar positionen på ett element i arrayen
+    public int getEmployeeID(int id) {
+        int position;
+        position = employeeID.indexOf(id);
+        return position;
+    }
+
+    //hämtar positionen på ett element i arrayen
+    public int getPassword(String id) {
+        int position;
+        position = password.indexOf(id);
+        return position;
+
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-    Employee employee = new Employee();
-    employee.employeeID=employee.generateEmployeeID();
-    System.out.println(employee.employeeID);
+        Employee employee = new Employee();
+        employee.employeeID = employee.generateEmployeeID();
+        System.out.println(employee.employeeID);
     }
 }
+
